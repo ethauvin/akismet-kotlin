7f3c783fe02cd7a114d1dc455c2bd6acbc59cb11
Erik C. Thauvin <erik@thauvin.net>
2021-03-21 17:14:41 -0700

    Updated copyright.

--- src/main/kotlin/net/thauvin/erik/akismet/AkismetComment.kt
+++ src/main/kotlin/net/thauvin/erik/akismet/AkismetComment.kt
@@ -1,7 +1,7 @@
 /*
  * AkismetComment.kt
  *
- * Copyright (c) 2019-2020, Erik C. Thauvin (erik@thauvin.net)
+ * Copyright (c) 2019-2021, Erik C. Thauvin (erik@thauvin.net)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
