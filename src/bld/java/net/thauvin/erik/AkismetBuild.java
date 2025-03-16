6e1f2991007747f2f9f1cf18efde1f03863faf1c
Erik C. Thauvin <erik@thauvin.net>
2025-03-16 11:21:35 -0700

    Bump JUnit to version 5.12.1

--- src/bld/java/net/thauvin/erik/AkismetBuild.java
+++ src/bld/java/net/thauvin/erik/AkismetBuild.java
@@ -84,8 +84,8 @@ public class AkismetBuild extends Project {
         scope(test)
                 .include(dependency("org.mockito", "mockito-core", version(5, 13, 0)))
                 .include(dependency("org.jetbrains.kotlin", "kotlin-test-junit5", kotlin))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 0)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 0)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 1)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 1)))
                 .include(dependency("com.willowtreeapps.assertk", "assertk-jvm", version(0, 28, 1)));
 
         publishOperation()
