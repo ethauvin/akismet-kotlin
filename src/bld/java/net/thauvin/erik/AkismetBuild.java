cb5cd8fc847b06c062592b0252bf4a3a398829cf
Erik C. Thauvin <erik@thauvin.net>
2025-03-16 12:16:33 -0700

    Update copyright

--- src/bld/java/net/thauvin/erik/AkismetBuild.java
+++ src/bld/java/net/thauvin/erik/AkismetBuild.java
@@ -1,7 +1,7 @@
 /*
  * AkismetBuild.java
  *
- * Copyright 2019-2024 Erik C. Thauvin (erik@thauvin.net)
+ * Copyright 2019-2025 Erik C. Thauvin (erik@thauvin.net)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
