b8a8529e4fd2ab9e7abf1e6e2f9daed9e8bff642
Erik C. Thauvin <erik@thauvin.net>
2025-04-13 13:39:12 -0700

    Bump Kotlinx Json Serialization to version 1.8.1

--- src/bld/java/net/thauvin/erik/AkismetBuild.java
+++ src/bld/java/net/thauvin/erik/AkismetBuild.java
@@ -79,7 +79,7 @@ public class AkismetBuild extends Project {
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", kotlin))
                 .include(dependency("com.squareup.okhttp3", "okhttp", okHttp))
                 .include(dependency("com.squareup.okhttp3", "logging-interceptor", okHttp))
-                .include(dependency("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.8.0"));
+                .include(dependency("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.8.1"));
         scope(provided)
                 .include(dependency("jakarta.servlet", "jakarta.servlet-api", version(6, 1, 0)));
         scope(test)
