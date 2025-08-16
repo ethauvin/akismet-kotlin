7f91b06b99b3a8b567326cafe167390bfdfb9533
Erik C. Thauvin <erik@thauvin.net>
2025-08-16 00:46:54 -0700

    Bump Gradle to version 9.0.0

--- examples/gradle/build.gradle.kts
+++ examples/gradle/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("application")
     id("com.github.ben-manes.versions") version "0.51.0"
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.2.10"
 }
 
 defaultTasks(ApplicationPlugin.TASK_RUN_NAME)
