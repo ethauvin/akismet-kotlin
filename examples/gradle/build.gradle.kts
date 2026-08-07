e990ca381635f0404dba5144971015adea2155ce
Erik C. Thauvin <erik@thauvin.net>
2026-08-07 12:36:30 -0700

    Bump Versions plugin to version 0.60.0

--- examples/gradle/build.gradle.kts
+++ examples/gradle/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("application")
-    id("io.github.ben-manes.versions") version "0.59.0"
+    id("io.github.ben-manes.versions") version "0.60.0"
     kotlin("jvm") version "2.4.10"
 }
 
