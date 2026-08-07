984f3ebd570fcd9ca9e76cc6d7c5311ef772d5a2
Erik C. Thauvin <erik@thauvin.net>
2026-08-06 21:08:16 -0700

    Bump OkHttp to version 5.4.0

--- src/bld/java/net/thauvin/erik/AkismetBuild.java
+++ src/bld/java/net/thauvin/erik/AkismetBuild.java
@@ -75,7 +75,7 @@ public class AkismetBuild extends Project {
         downloadSources = true;
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES);
 
-        var okHttp = version(5, 3, 2);
+        var okHttp = version(5, 4, 0);
         var junit = version(6, 0, 3);
         var kotlin = version(2, 4, 10);
         scope(compile)
