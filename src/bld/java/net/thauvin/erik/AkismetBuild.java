7f09a7856ebd4f373cf6dd4d0400fa06ebda53b9
Erik C. Thauvin <erik@thauvin.net>
2026-08-06 21:12:42 -0700

    Bump SpotBugs Annotations to version 4.10.3

--- src/bld/java/net/thauvin/erik/AkismetBuild.java
+++ src/bld/java/net/thauvin/erik/AkismetBuild.java
@@ -89,7 +89,7 @@ public class AkismetBuild extends Project {
         scope(provided)
                 .include(dependency("jakarta.servlet", "jakarta.servlet-api", version(6, 1, 0)))
                 .include(dependency("com.github.spotbugs", "spotbugs-annotations",
-                        version(4, 9, 8)));
+                        version(4, 10, 3)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
                         version(0, 9, 6)))
